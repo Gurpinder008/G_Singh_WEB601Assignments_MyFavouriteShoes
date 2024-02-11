a9344ab8957b2961c7b7d6fd59d295f84f544a41
Gurpinder Singh <133062008+Gurpinder008@users.noreply.github.com>
2024-02-10 19:48:09 -0500

    commit

---
+++ G_Singh_MyFavouriteShoes/src/app/shoe-type.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ShoeTypePipe } from './shoe-type.pipe';
+
+describe('ShoeTypePipe', () => {
+  it('create an instance', () => {
+    const pipe = new ShoeTypePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
